9a30c0de0354114afe6777e916f2f30ce4d961b2
canhquydev <canhquydev@hotmail.com>
2025-05-17 07:36:42 +0700

    update dockerfile

--- app.py
+++ app.py
@@ -446,5 +446,5 @@ def update_link(original_link, new_pkg, new_code):
 
 if __name__ == '__main__':
     init_db()
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
